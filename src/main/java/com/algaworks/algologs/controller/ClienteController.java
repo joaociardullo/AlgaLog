010aa7b0a142a68c8beae8db3930fcf6fbc44905
Joao Vitor de Jesus Ciardullo <joao.temp@icesp.local>
2021-10-20 16:12:46 -0300

    Criando banco de dados e seu propriedade so spring

--- src/main/java/com/algaworks/algologs/controller/ClienteController.java
+++ src/main/java/com/algaworks/algologs/controller/ClienteController.java
@@ -3,36 +3,25 @@ package com.algaworks.algologs.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algologs.model.Cliente;
 
-@RestController
+@RestController /*Rest*/
 //Fazendo teste com postman 
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		//criando dois clientes para teste do postman
-		cliente1.setId(1l);
-		cliente1.setNome("Joao");
-		cliente1.setTelefone("11985640");
-		cliente1.setEmail("joao@hotmail.com");
-		
-		
-		
-	var cliente2 = new Cliente();
-		
-		cliente1.setId(2l);
-		cliente1.setNome("maria");
-		cliente1.setTelefone("11985640");
-		cliente1.setEmail("maria@hotmail.com");
-		
-		
-		
-		
-		return Arrays.asList(cliente1,cliente2);
-	}
+		return manager.createNamedQuery("FROM Cliente", Cliente.class) //consulta jpql | result classe tipagem para retonar cliente 
+				.getResultList(); //retorna uma lista via get
+				
+		}
 }
--- src/main/java/com/algaworks/algologs/model/Cliente.java
+++ src/main/java/com/algaworks/algologs/model/Cliente.java
@@ -1,48 +1,84 @@
 package com.algaworks.algologs.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter; //para facilitar os construtores
+
+//@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
+//Declarando as variaveis 
 
+	@EqualsAndHashCode.Include
+	@Id // Chave primary
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // forma nativa do banco de dados
 	private Long id;
+
+	@Column // adicionar na coluna ->
 	private String nome;
 	private String email;
 	private String telefone;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
-	
-	
 
-	
-		
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
+
+}
